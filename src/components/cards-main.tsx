340da4a58d458369fea8199f35568976e1cad7a2
saimonbrito <britomauricio89@gmail.com>
2025-02-03 11:34:17 -0300

    passando propriedade via props e criando mais componentes   update

--- src/components/cards-main.tsx
+++ src/components/cards-main.tsx
@@ -1,17 +1,18 @@
 
 
-import artis from './assets/playlist/1.jpeg'
+
 import styles from './cards-main.module.css'
 interface cardProps{
     title: string
     name: string
+    imegem: string
 }
 
-export function CardsMain({title,name}:cardProps){
+export function CardsMain({title,name,imegem}:cardProps){
     return(
       
-         <div className={styles.cardCar}>
-            <img className={styles.cardCarImg} src={artis} alt="artista" />
+         <div className={styles.mainCardCar}>
+            <img className={styles.mainCardCarImg} src={imegem} alt="artista" />
             <span>{title}</span>
             <p>{name}</p>
         </div>
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,6 +1,11 @@
 
 import  { CardsMain } from './cards-main'
 import { MainHeader } from './main-header'
+import artis1 from './assets/playlist/1.jpeg'
+import artis2 from './assets/playlist/9.jpeg'
+import artis3 from './assets/playlist/5.jpeg'
+import artis4 from './assets/playlist/11.jpeg'
+
 import styles from './main.module.css'
 
 
@@ -14,18 +19,22 @@ export function Main(){
            <CardsMain 
                 title="Christmas Hits" 
                 name="Artista"
+                imegem={artis1}
             />
            <CardsMain
                 title='simbady' 
                 name='rits'
+                imegem={artis2}
             />
            <CardsMain 
                 title='sempr'
                 name='kid'
+                imegem={artis3}
              />
            <CardsMain 
                 title='futuros' 
                 name='test'
+                imegem={artis4}
            />
 
            </div>
